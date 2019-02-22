a7ec78b8f19dab55d7d7794b9911b328f6497f06
v_zhangwenzhao <v_zhangwenzhao@bilibili.com>
2019-02-22 18:03:51 +0800

    template finish

---
+++ src/views/layout/components/BreadCrumb/BreadCrumb.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="omega-com-bread-crumb-container">
+    <el-breadcrumb class="omega-com-bread" separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :key="index">
+        应用管理
+      </el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+.omega-com-bread-crumb-container {
+  height: 100%;
+  width: 100%;
+  position: relative;
+  .omega-com-bread {
+    position: absolute;
+    top: 50%;
+    left: 0px;
+    transform: translateY(-50%);
+  }
+}
+</style>
--- src/views/layout/components/Main.vue
+++ src/views/layout/components/Main.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="omega-main-container">
-    为什么
+    <div class="omega-bread-container">
+      <Bread />
+    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
+import Bread from './BreadCrumb/BreadCrumb.vue'
 export default {
-
+  components: {
+    Bread
+  }
 }
 </script>
 
 <style lang="less">
 .omega-main-container {
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  .omega-bread-container {
+    height: 50px;
+    padding: 0px 40px;
+    flex-shrink: 0;
+  }
 }
 </style>
